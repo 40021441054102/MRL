d0a01cf1b32b6ed8ff8c24f76af66c3ba55c4c5f
RamtinKosari <ramtinkosari@gmail.com>
2023-09-10 00:19:40 +0330

    Add Character Class

--- QMRLT/Robot/Connection/ascii-camera/asciiCamera.hpp
+++ QMRLT/Robot/Connection/ascii-camera/asciiCamera.hpp
@@ -11,6 +11,7 @@
     # include <opencv4/opencv2/imgproc.hpp>
     # include <opencv4/opencv2/highgui.hpp>
     # include <iostream>
+    # include <string>
     //-- Check Operating System for Specific Escape Character
     # if defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__) || defined(__MACH__) || defined(macintosh) || defined(Macintosh)
         /**
@@ -31,4 +32,38 @@
          */
         std::string escChar = "\1xb[";
     # endif // Check Operating System for Console Escape Character
+    /**
+     * @brief Class Definition of Colorful Characters
+     */
+    class Character {
+        protected:
+            /**
+             * @brief Ascii Pixel
+             */
+            std::string ascii_pixel;
+            /**
+             * @brief Structure to Store RGB's Value of Ascii Pixel
+             */
+            struct Color {
+                int green;
+                int blue;
+                int red;
+            };
+            /**
+             * @brief Object to Access RGB's Value of Ascii Pixel
+             */
+            Color color;
+        public:
+            /**
+             * @brief Constructor
+             */
+            Character() noexcept;
+            /**
+             * @brief Method to Set RGB Value of Ascii Pixel
+             * @param _blue Blue
+             * @param _green Green
+             * @param _red Red
+             */
+            void setRGB(int& _blue, int& _green, int& _red) noexcept;
+    };
 # endif // __QMRLT_ASCII_IMAGE
